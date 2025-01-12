d245fe70d2c73b74372c08df3d9fb5dfc58e2f8b
Berindeiu Sergiu <108501624+BerindeiuSergiu@users.noreply.github.com>
2025-01-12 17:39:32 +0200

    added one more unpacking method

--- unpacker.py
+++ unpacker.py
@@ -73,7 +73,7 @@ def unpack_3127opcode_2625control_bits_2420source_register_1915source_register_1
     instruction = 1081282099
 
     _10alignment = instruction & 0b11
-    _62opcode = (instruction >> 2) & 0b111111
+    _62opcode = (instruction >> 2) & 0b11111
     _117destination_register = (instruction >> 7) & 0b11111
     _1412function = (instruction >> 12) & 0b111
     _1915source_register = (instruction >> 15) & 0b11111
@@ -108,8 +108,9 @@ def unpack_3127opcode_2625control_bits_2420source_register_1915source_register_1
         "control_bits": f"{_2625control_bits:07b}",
         "source_register_1": rs1,
         "source_register_2": rs2,
+        "function": f"{_1412function:03b}",
         "destination_register": rd,
-        "function": f"{_62opcode:05b}",
+        "opcode": f"{_62opcode:05b}",
         "alignment": f"{_10alignment:02b}",
         "decoded_function": instruction_name
     }
@@ -118,4 +119,67 @@ def unpack_3127opcode_2625control_bits_2420source_register_1915source_register_1
     
     return decoded_instruction
 
-unpack_3127opcode_2625control_bits_2420source_register_1915source_register_1412function_117destination_register_62opcode_10alignment(mem, 0)
+def remove_trailing_zeros_and_revert(binary_value):
+    binary_str = bin(binary_value)[2:]
+    
+    
+    cleaned_binary = binary_str[::-1].lstrip('0')[::-1]
+    reverted_binary = cleaned_binary[::-1]
+    
+    return int(reverted_binary, 2) if reverted_binary else 0
+
+# addi t2, t1, 0x5 => 2684552083 #
+# addi t2, t1, 0x27(39) => 3825402771 #
+# addi t2, t1, 0x335(821) => 2898461587 #
+
+
+def unpack__3120immediate_1915source_register_1412function_117destination_register_62opcode_10alignment(mem, address):
+    instruction = 2684552083
+
+    print(f"{instruction:32b}")
+    
+    _10alignment = instruction & 0b11
+    _62opcode = (instruction >> 2) & 0b11111
+    _117destination_register = (instruction >> 7) & 0b11111
+    _1412function = (instruction >> 12) & 0b111
+    _1915source_register = (instruction >> 15) & 0b11111
+    _3120immediate_value = (instruction >> 20) & 0b111111111111 
+    
+    value = remove_trailing_zeros_and_revert(_3120immediate_value)
+    print(_3120immediate_value)
+    rs1 = [k for k, v in abi_names.items() if v == _1915source_register][0]
+    rd = [k for k, v in abi_names.items() if v == _117destination_register][0]
+
+    opcode_bin = f"{_62opcode:05b}" + f"{_10alignment:02b}"
+    instruction_name = None
+
+    if opcode_bin in instruction_set:
+        if isinstance(instruction_set[opcode_bin], dict):
+            func_bin = f"{_1412function:03b}"
+            if func_bin in instruction_set[opcode_bin]:
+                instruction_name = instruction_set[opcode_bin][func_bin]
+            else:
+                instruction_name = "Unknown function"
+        else:
+            instruction_name = instruction_set[opcode_bin]
+
+    if instruction_name is None:
+        instruction_name = "Unknown opcode"
+
+
+    decoded_instruction = {
+        "immediate_value": f"{value}",
+        "source_register_1": rs1,
+        "function":f"{_1412function:03b}",
+        "destination_register": rd,
+        "opcode": f"{_62opcode:05b}",
+        "alignment": f"{_10alignment:02b}",
+        "decoded_function": instruction_name
+    }
+
+    print(decoded_instruction)
+    
+    return decoded_instruction
+
+
+unpack__3120immediate_1915source_register_1412function_117destination_register_62opcode_10alignment(mem, 0)
