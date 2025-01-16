9b9c33beaf12044c472af286dc59e1c36620646a
vcarla <carla.vekony@student.upt.ro>
2025-01-16 21:18:14 +0200

    alled tests

---
+++ works_count.py
@@ -0,0 +1,13 @@
+'lui', 'auipc', 
+'addi', 'slti', 'sltiu', 'xori', 'ori', 'andi',
+'slli', 'srli', 'srai',
+'add', 'sub', 'sll', 'slt', 'sltu', 'xor', 'srl', sra, 'or', 'and', 'mul', mulh, mulhsu, mulhu, 'div', 'divu', 'rem', 'remu',
+'jal', 'jalr',
+'sb', 'sh', 'sw',
+'beq', 'bne', 'blt', 'bge', 'bltu', 'bgeu',
+'push', 'pop',
+'lb', 'lh', 'lw', 'lbu', 'lhu',
+'amoswap.w', 'amoadd.w', 'amoxor.w', 'amoand.w', 'amoor.w', 'amomin.w', 'amomax.w', 'amominu.w', 'amomaxu.w'
+'ret', 'ecall', 'ebreak', 'uret', 'sret', 'mret',  'wfi',
+
+
